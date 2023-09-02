6af6a02def5b3b028d8b607222d41ad50a8dcf7e
bartek1190 <bartlomiej.m.michonski@gmail.com>
2023-09-02 10:29:13 +0200

    first exercise DOM

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+"use strict";
+const categoryList = document.querySelector("#categories");
+const categoryItems = categoryList.querySelectorAll("li.item");
+const numberOfCategories = categoryItems.length;
+console.log("Number of categories: " + numberOfCategories);
+
+categoryItems.forEach((item) => {
+  const categoryTitle = item.querySelector("h2").textContent;
+  const categoryElements = item.querySelectorAll("li").length;
+  console.log("Category: ", categoryTitle);
+  console.log("Elements: ", categoryElements);
+});
