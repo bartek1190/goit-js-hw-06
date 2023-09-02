b2065283b73cac5c8b8857b2cf651ff99ffbcaea
bartek1190 <bartlomiej.m.michonski@gmail.com>
2023-09-02 13:50:02 +0200

    exercise 8 i 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const emailInput = loginForm.elements.email;
+  const passwordInput = loginForm.elements.password;
+
+  if (emailInput.value.trim() === "" || passwordInput.value.trim() === "") {
+    alert("Wypełnij wszystkie pola formularza.");
+  } else {
+    const formData = {
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
+    console.log(formData);
+    loginForm.reset();
+  }
+});
