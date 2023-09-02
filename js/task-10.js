68cd5b1bbaedfd3600920d3a5b409a6f67886bcb
bartek1190 <bartlomiej.m.michonski@gmail.com>
2023-09-02 13:52:57 +0200

    exercise 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,41 @@
+const controlsDiv = document.getElementById("controls");
+const input = controlsDiv.querySelector("input");
+const createButton = controlsDiv.querySelector("[data-create]");
+const destroyButton = controlsDiv.querySelector("[data-destroy]");
+const boxesDiv = document.getElementById("boxes");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, "0")}`;
 }
+
+function createBoxes(amount) {
+  const boxes = [];
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+
+  boxesDiv.append(...boxes);
+}
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = "";
+}
+
+createButton.addEventListener("click", () => {
+  const amount = parseInt(input.value);
+
+  if (amount > 0) {
+    createBoxes(amount);
+  }
+});
+
+destroyButton.addEventListener("click", () => {
+  destroyBoxes();
+});
