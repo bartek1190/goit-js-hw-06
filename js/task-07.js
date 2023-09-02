54a96d47ecd0c64ec11d1431cda986c554d13509
bartek1190 <bartlomiej.m.michonski@gmail.com>
2023-09-02 13:43:58 +0200

    exercise 7

---
+++ js/task-07.js
@@ -0,0 +1,6 @@
+const fontSizeControl = document.getElementById("font-size-control");
+const textSpan = document.getElementById("text");
+fontSizeControl.addEventListener("input", () => {
+  const fontSizeValue = fontSizeControl.value;
+  textSpan.style.fontSize = fontSizeValue + "px";
+});
